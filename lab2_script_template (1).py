95c2a7d1cf229bdd50ae002059b7dbd910229932
pipiyus <156553508+pipiyus@users.noreply.github.com>
2024-06-07 19:54:45 -0400

    Initial commit

---
+++ lab2_script_template (1).py
@@ -0,0 +1,29 @@
+def main():
+
+    # TODO: Step 2 - Create a complex data structure
+    about_me = {}
+
+    # TODO: Step 3 - Add another movie to the data structure
+    
+# TODO: Step 4 - Function that prints student name and ID	
+def print_student_name_and_id(about_me):
+    return
+    
+# TODO: Step 5 - Function that adds pizza toppings to data structure
+def add_pizza_toppings(about_me, toppings):
+    return
+
+# TODO: Step 6 - Function that prints bullet list of pizza toppings
+def print_pizza_toppings(about_me):
+    return
+
+# TODO: Step 7 - Function that prints comma-separated list of movie genres
+def print_movie_genres(about_me):
+    return 
+
+# TODO: Step 8 - Function that prints comma-separated list of movie titles
+def print_movie_titles(movie_list):
+    return
+    
+if __name__ == '__main__':
+    main()
